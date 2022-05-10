2e3f1e56e34be083fbd1965afb3210acfa870f24
YA <YA-androidapp@users.noreply.github.com>
2022-05-10 20:39:56 +0900

    init

---
+++ Otowa/OtowaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtowaApp.swift
+//  Otowa
+//
+//  Created by Yu on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct OtowaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Otowa-iOS
+
+---
+
+[Otowa](https://github.com/YA-androidapp/Otowa) をiOS向けに移植
+
+---
+
+Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
