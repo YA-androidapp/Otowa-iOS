9b1cec9bebd6700b24bfa895ebcd299ed231ed2a
YA <YA-androidapp@users.noreply.github.com>
2022-05-14 23:55:40 +0900

    Update TwitterAccess.swift

--- Otowa/TwitterAccess.swift
+++ Otowa/TwitterAccess.swift
@@ -9,20 +9,27 @@ import Foundation
 import TwitterAPIKit
 
 func getClient() -> TwitterAPIKit? {
+    var client :TwitterAPIKit? = nil
+    
     if(
         UserDefaults.standard.string(forKey: "ck_preference") != "" &&
         UserDefaults.standard.string(forKey: "cs_preference") != "" &&
         UserDefaults.standard.string(forKey: "ot_preference") != "" &&
         UserDefaults.standard.string(forKey: "os_preference") != ""
     ) {
-        return TwitterAPIKit(
-            .oauth(
-                consumerKey: UserDefaults.standard.string(forKey: "ck_preference") ?? "",
-                consumerSecret: UserDefaults.standard.string(forKey: "cs_preference") ?? "",
-                oauthToken: UserDefaults.standard.string(forKey: "ot_preference") ?? "",
-                oauthTokenSecret: UserDefaults.standard.string(forKey: "os_preference") ?? ""
+        do {
+            client = try TwitterAPIKit(
+                .oauth(
+                    consumerKey: UserDefaults.standard.string(forKey: "ck_preference") ?? "",
+                    consumerSecret: UserDefaults.standard.string(forKey: "cs_preference") ?? "",
+                    oauthToken: UserDefaults.standard.string(forKey: "ot_preference") ?? "",
+                    oauthTokenSecret: UserDefaults.standard.string(forKey: "os_preference") ?? ""
+                )
             )
-        )
+            return client
+        } catch {
+            return nil
+        }
     } else {
         return nil
     }
